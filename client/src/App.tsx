f75c69368b7ea42ee064b53f93506bab48f3d9ee
deejeexo <gotenks257@gmail.com>
2023-02-10 22:12:52 +0200

    Implemented routering in react

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,16 @@
 import ClientPage from "./pages/ClientPage";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
-  return <ClientPage></ClientPage>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<ClientPage />} />
+        <Route path="/signup" element={<div></div>} />
+        <Route path="/login" element={<div></div>} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- client/src/components/LandingPage/MainSection.tsx
+++ client/src/components/LandingPage/MainSection.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 function MainSection() {
+  const navigate = useNavigate();
+
   return (
     <div>
       <section className="mb-6">
@@ -71,6 +74,9 @@ function MainSection() {
                 className="inline-block px-6 py-2.5 mr-2 bg-transparent text-blue-600 font-medium text-xs leading-tight uppercase rounded hover:text-blue-700 hover:bg-gray-100 focus:bg-gray-100 focus:outline-none focus:ring-0 active:bg-gray-200 transition duration-150 ease-in-out"
                 data-mdb-ripple="true"
                 data-mdb-ripple-color="light"
+                onClick={() => {
+                  navigate("/login");
+                }}
               >
                 Login
               </button>
@@ -79,6 +85,9 @@ function MainSection() {
                 className="inline-block px-6 py-2.5 bg-blue-600 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg transition duration-150 ease-in-out"
                 data-mdb-ripple="true"
                 data-mdb-ripple-color="light"
+                onClick={() => {
+                  navigate("/signup");
+                }}
               >
                 Sign up for free
               </button>
